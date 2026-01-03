a71c332004f7da35596b7e14dd44ffe1b30b37f1
Ori Elbaz <83533131+OriElbaz@users.noreply.github.com>
2026-01-03 14:00:18 +1100

    test fastapi end point

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import tensorflow as tf
 from predict import predict_image
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
 
 def load_model(model_path: str):
     return tf.keras.models.load_model(model_path)
- 
 
-def main():
-    img_file_path = "images/test-img-8.png"
+ 
+@app.get("/")
+def predict():
+    img_file_path = "images/img-seven.png"
     model = load_model("models/digit-guesser-softmax.keras")
     digit, confidence = predict_image(img_file_path, model)
-    print(f"Predicted Digit: {digit}, Confidence: {confidence*100:.2f}%")
+    return(f"Predicted Digit: {digit}, Confidence: {confidence*100:.2f}%")
 
-
-if __name__ == "__main__":
-    main()
